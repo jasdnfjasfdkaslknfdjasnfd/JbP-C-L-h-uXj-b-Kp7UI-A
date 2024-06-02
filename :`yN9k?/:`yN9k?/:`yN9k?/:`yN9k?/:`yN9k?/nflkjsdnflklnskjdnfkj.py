9111a3bfecf66bb49c8ac1f59e06e92edf35855b
jasdnfjasfdkaslknfdjasnfd <iutmytrbvsr.irymutneb@interia.pl>
2024-06-02 04:13:35 +0200

    Update nflkjsdnflklnskjdnfkj.py

--- :`yN9k?/:`yN9k?/:`yN9k?/:`yN9k?/:`yN9k?/nflkjsdnflklnskjdnfkj.py
+++ :`yN9k?/:`yN9k?/:`yN9k?/:`yN9k?/:`yN9k?/nflkjsdnflklnskjdnfkj.py
@@ -1,4 +1,38 @@
 import os
+import time
 
-os.system("cls")
-print("TEST")
+os.system('cls')
+os.system('title BlackWall')
+
+logo = r"""
+
+
+
+
+
+
+
+
+
+
+                                     ____  _            _     __        __    _ _ 
+                                    | __ )| | __ _  ___| | __ \ \      / /_ _| | |
+                                    |  _ \| |/ _` |/ __| |/ /  \ \ /\ / / _` | | |
+                                    | |_) | | (_| | (__|   <    \ V  V / (_| | | |
+                                    |____/|_|\__,_|\___|_|\_\    \_/\_/ \__,_|_|_|
+
+
+
+
+
+
+
+
+
+
+                                               
+"""
+
+print(logo)
+time.sleep(2)
+input()
